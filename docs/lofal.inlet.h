c2245c558f2fdce7bb9a01250c1f0144f7650c01
A Taylor <112668339+ATaylorAerospace@users.noreply.github.com>
2023-10-26 20:44:40 -0700

    Create lofal.inlet.h

---
+++ docs/lofal.inlet.h
@@ -0,0 +1,20 @@
+#ifndef INLET_H
+#define INLET_H
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define MAX_THROTTLE 1000
+#define MIN_THROTTLE 10
+#define MAX_VELOCITY 100
+#define MIN_VELOCITY 5
+
+using namespace std;
+
+struct Inlet {
+    double throttle;
+    double velocity;
+};
+
+#endif // INLET_H
