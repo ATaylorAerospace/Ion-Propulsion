811aae4a9eb3f525515d6ef69f24cc1645e76ba0
A Taylor <112668339+ATaylorAerospace@users.noreply.github.com>
2023-11-12 15:19:29 -0800

    Update and rename OrbitalVelocity to orbitalvelocity.py

--- python/keplerleo/orbitalvelocity.py
+++ python/keplerleo/orbitalvelocity.py
@@ -1,11 +1,11 @@
-class calculateOrbitalVelocity:
+class calculate orbitalvelocity:
     def __init__(self, a, ecc, inc, time):
         self.a = a
         self.ecc = ecc
         self.inc = inc
         self.time = time
 
-    def calculateOrbitalVelocity(self):
+    def calculate orbitalvelocity(self):
         # Formula: Orbital Velocity = (GM / a)^(1/2)
         orbital_velocity = (G * self.a ** 2) ** (1/2)
         return orbital_velocity
