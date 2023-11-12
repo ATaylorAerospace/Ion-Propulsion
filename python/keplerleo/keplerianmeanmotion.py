edeff500bd4b8eb2aa62a635eeb6c748a0aea192
A Taylor <112668339+ATaylorAerospace@users.noreply.github.com>
2023-11-12 15:17:33 -0800

    Update and rename KeplerianMeanMotion to keplerianmeanmotion.py

--- python/keplerleo/keplerianmeanmotion.py
+++ python/keplerleo/keplerianmeanmotion.py
@@ -1,11 +1,11 @@
-class calculateKeplerianMeanMotion:
+class calculate keplerianmeanmotion:
     def __init__(self, a, ecc, inc, time):
         self.a = a
         self.ecc = ecc
         self.inc = inc
         self.time = time
 
-    def calculateKeplerianMeanMotion(self):
+    def calculate keplerianmeanmotion(self):
         # Formula: Mean Motion = 0.917 * (a ** 3) / (1 + ecc * cos(inc))
         mean_motion = 0.917 * (self.a ** 3) / (1 + self.ecc * math.cos(self.inc))
         return mean_motion
